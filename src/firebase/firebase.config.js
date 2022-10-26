74a65b867a30ff8c8f6ad3cfc86a5d3d498f919b
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-26 10:10:17 +0600

    added some file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCt3Uavt7qnu6d09HhqQ5HpThuPdf5RWto",
+  authDomain: "cicada-courses.firebaseapp.com",
+  projectId: "cicada-courses",
+  storageBucket: "cicada-courses.appspot.com",
+  messagingSenderId: "792237970730",
+  appId: "1:792237970730:web:be2248b24009912e3bfa53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
