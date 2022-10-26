6550e4c1bb984c99c12f2814d1580608dbc9e681
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-26 10:42:48 +0600

    create not found page and sytle the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layouts/Main';
+import NotFoundPage from './Pages/404/NotFoundPage';
+import Blog from './Pages/Blog/Blog';
+import Courses from './Pages/Courses/Courses';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
 import Navbar from './Pages/Navbar/Navbar';
 
 function App() {
+
+  const router = createBrowserRouter([
+      {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+          {
+            path:'/',
+            element:<Home></Home>
+          },
+          {
+            path:'/courses',
+            element:<Courses></Courses>
+          },
+          {
+            path:'/blog',
+            element:<Blog></Blog>
+          },
+          {
+            path:'/login',
+            element:<Login></Login>
+          },
+          {
+            path:'/register',
+            element:<Register></Register>
+          },
+          {
+            path:'*',
+            element:<NotFoundPage></NotFoundPage>
+          }
+        ]
+      }
+  ])
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -3,37 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 justify-around">
+        <div className="navbar bg-primary justify-around">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/courses'>Courses</Link></li>
+                        <li><Link to='/faq'>FAQ</Link></li>
+                        <li><Link to='/blog'>Blog</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Cicada Courses</a>
             </div>
 
             <div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><Link>Home</Link></li>
-                        <li><Link>Courses</Link></li>
-                        <li><Link>FAQ</Link></li>
-                        <li><Link>Blog</Link></li>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/courses'>Courses</Link></li>
+                        <li><Link to='/faq'>FAQ</Link></li>
+                        <li><Link to='/blog'>Blog</Link></li>
                     </ul>
                 </div>
                 {/* <button className="btn btn-ghost btn-circle">
