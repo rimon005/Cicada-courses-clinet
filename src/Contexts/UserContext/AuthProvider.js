dba307b8996d1e98d727ce739940b2c1f05b470a
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-26 11:24:52 +0600

    create context

---
+++ src/Contexts/UserContext/AuthProvider.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { createContext } from 'react';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+import app from '../../firebase/firebase.config';
+import { useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider()
+    const googleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    }
+
+    const verifyEmail = () => {
+        return sendEmailVerification(auth.currentUser)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log(currentUser);
+            if (currentUser === null || currentUser.emailVerified) {
+                setUser(currentUser);
+            }
+            setLoading(false)
+        })
+
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+
+
+    const authInfo = { user, googleSignIn, signIn, createUser, logOut, loading,setLoading, updateUserProfile, verifyEmail }
+
+    return (
+        <AuthContext.Provider value={authInfo} >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
