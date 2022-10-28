5e97ed6fcc1306d66b3138a94274c51a74742fa5
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-28 17:13:19 +0600

    added env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCt3Uavt7qnu6d09HhqQ5HpThuPdf5RWto",
-  authDomain: "cicada-courses.firebaseapp.com",
-  projectId: "cicada-courses",
-  storageBucket: "cicada-courses.appspot.com",
-  messagingSenderId: "792237970730",
-  appId: "1:792237970730:web:be2248b24009912e3bfa53"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
